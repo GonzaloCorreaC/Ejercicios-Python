cbc54b05a5d23e67197540018e4bcc8e3afc5e06
Gonzalo Correa <gonzalocorrea111@gmail.com>
2026-05-05 14:42:50 -0400

    Nuevo archivo

---
+++ ejemplos.py
@@ -0,0 +1,56 @@
+##ej1
+#medicamentos = 60000
+#despacho = 8000
+
+#edad = int(input("Ingrese su edad: "))
+#tramo = input("Ingrese al tramo en el que pertenece (A/B/C/D): ")
+#tramo = tramo.upper()
+
+#if edad <= 30:
+    
+    #if tramo == "A" or tramo == "B":
+        #medicamentos_final = medicamentos * 0.82   
+    #else:
+        #medicamentos_final = medicamentos * 0.88
+       
+#elif edad <= 60:
+
+    #if tramo == "A" or tramo == "B":
+        #medicamentos_final = medicamentos * 0.88
+    #else:
+        #medicamentos_final = medicamentos * 0.92    
+
+#else:
+    #medicamentos_final = medicamentos
+
+#print(f"El precio final de sus medicamentos es de: {int(medicamentos_final)}")
+
+#if (tramo == "A" or tramo == "B") and edad >= 55:
+    #despacho_final = despacho * 0.85
+
+#elif tramo == "A" or tramo == "B":
+    #despacho_final = despacho * 0.90
+
+#else:
+    #despacho_final = despacho
+   
+#print(f"El precio final de su despacho es de: {int(despacho_final)}")
+
+##ej2
+#from random import randint
+
+#num1 = int(input("Ingrese el primer numero: "))
+#num2 = int(input("Ingrese el segundo numero: "))
+
+#if num1 >= num2:
+    #print("El primer valor debe ser menor que el segundo")
+
+#else:
+    #numero = randint(num1, num2)
+    #print(f"Numero generado entre {num1} y {num2} es: {numero}")
+
+
+
+
+
+
