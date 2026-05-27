179c2c2ff3e68a1f49e0ec9a23112ddc57d95993
Gonzalo Correa <gonzalocorrea111@gmail.com>
2026-05-27 19:40:07 -0400

    Nuevo archivo

---
+++ EjerciciosCiclosExcept.py
@@ -0,0 +1,33 @@
+#correo_insti = 0
+#correo_no_insti = 0
+
+#while True:
+    #try:
+        #cantidad_revisar = int(input("Ingresa la cantidad de correos a revisar:"))
+        
+        #if cantidad_revisar > 0:
+            #break
+        #else:
+            #print("Ingrese numeros enteros mayores a cero")    
+        
+    #except ValueError:
+        #print("Ingrese numeros enteros, positivos a cero")
+
+#for i in range(cantidad_revisar):
+    #while True:
+        #correo = input("Ingrese correo: ").strip().lower()
+
+        #if "@" in correo and " " not in correo:
+            #break
+        #else:
+            #print("Correo debe contener @ y no tener espacios")
+    
+    #if correo.endswith("duoc.cl"):
+        #correo_insti += 1
+    #else:
+        #correo_no_insti += 1 
+
+#print(f"Correos institucionales: {correo_insti}")
+#print(f"Correos no institucionales: {correo_no_insti}")
+
+##ej2
